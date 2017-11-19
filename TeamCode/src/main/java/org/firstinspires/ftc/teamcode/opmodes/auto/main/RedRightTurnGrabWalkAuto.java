7ccaf5299f097cda892af28d288941fda4050b81
Yusuf Karim <ykarim250@gmail.com>
2017-11-18 22:50:14 -0500

    Turn Grab Walk autonomous modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/main/RedRightTurnGrabWalkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/main/RedRightTurnGrabWalkAuto.java
@@ -6,31 +6,34 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.robot.Directions;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
-@Autonomous(name = "Straight Grab Walk", group = "auto")
-public class StraightGrabWalkAuto extends LinearOpMode {
+@Autonomous(name = "Red Right Turn Grab Walk", group = "auto")
+public class RedRightTurnGrabWalkAuto extends LinearOpMode {
 
     private Robot robot = new Robot();
 
     @Override
     public void runOpMode() throws InterruptedException {
-        telemetry.addData("Path:", "Close arms, 20 inches to Safe Zone, Open Arms, Back 5 inches");
+        telemetry.addData("Path:", "Close arms, 20 inches north, 90 CW, Open Arms, 20 inches north, Back 5 inches");
         robot.init(hardwareMap);
 
         telemetry.addData("Status:", "Initialized");
         telemetry.update();
 
-//        AutoTransitioner.transitionOnStop(this, "Main TeleOp");
         waitForStart();
 
         robot.closeGlyphArms();
+        //Raise lift or no?
+
         robot.move(this, Directions.NORTH, 20);
+        robot.imuRotate(90, Directions.ROTATE_CW, 5);
         robot.openGlyphArms();
+        robot.move(this, Directions.NORTH, 20);
         robot.move(this, Directions.SOUTH, 5);
 
         telemetry.addData("Status:", "Done with Auto");
 //        telemetry.addData("Status:", "Transitioning to TeleOp");
         telemetry.update();
 
-        stop(); //Remove if AutoTrans in use
+        stop();
     }
 }
