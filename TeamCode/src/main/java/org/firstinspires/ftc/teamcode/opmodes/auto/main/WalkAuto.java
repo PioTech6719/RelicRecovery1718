934475ad45800fbab5662f9ff45c9e013515a139
Yusuf Karim <ykarim250@gmail.com>
2017-11-18 21:01:50 -0500

    Removed AutoTransitioner from WalkAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/main/WalkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/main/WalkAuto.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Directions;
 import org.firstinspires.ftc.teamcode.Robot;
-import org.firstinspires.ftc.teamcode.utils.AutoTransitioner;
 
 @Autonomous(name = "Auto Safe Zone Path", group = "main")
 public class WalkAuto extends LinearOpMode {
@@ -18,15 +17,11 @@ public class WalkAuto extends LinearOpMode {
         telemetry.update();
 
         robot.init(hardwareMap);
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
+
         telemetry.addData("Status:", "Initialized");
         telemetry.update();
 
-        AutoTransitioner.transitionOnStop(this, "Main TeleOp");
+//        AutoTransitioner.transitionOnStop(this, "Main TeleOp"); - Not tested
         waitForStart();
 
         robot.move(this, Directions.NORTH, 20);
