c06adf6ddf71cbc1760c0034292c614ddc95177f
Yusuf Karim <ykarim250@gmail.com>
2017-12-03 22:49:25 -0500

    REV Color Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/test/REVColorTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmodes.tele.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+@TeleOp(name = "Sensor - Color Sensor", group = "test")
+public class REVColorTest extends OpMode {
+
+    private Robot robot = new Robot();
+
+    @Override
+    public void init() {
+        robot.initTele(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Color Red : ", robot.colorSensor.red());
+        telemetry.addData("Color Blue : ", robot.colorSensor.blue());
+        telemetry.addData("Color Green : ", robot.colorSensor.green());
+        telemetry.addData("Color Alpha : ", robot.colorSensor.alpha());
+        telemetry.addData("Color ARGB : ", robot.colorSensor.argb());
+        telemetry.update();
+    }
+}
