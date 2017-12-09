aeadf102a153249b2989307cb74f869ea33c5d2d
Yusuf Karim <ykarim250@gmail.com>
2017-12-09 17:36:28 -0500

    RobotStates enum which may be used later

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotStates.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+//makes it more robot_specific which isnt desired
+public enum RobotStates {
+
+    DRIVE_FORWARD,
+    DRIVE_REVERSE,
+    DRIVE_LEFT,
+    DRIVE_RIGHT,
+    DRIVE_BRAKE,
+
+    ROTATE_CLOCKWISE,
+    ROTATE_COUNTERCLOCKWISE,
+    ROTATE_BRAKE,
+
+    SLOW,
+
+    RAISE_LIFT,
+    LOWER_LIFT,
+
+    INCREMENT_LIFT,
+    DECREMENT_LIFT,
+
+    OPEN_LOWER_GLYPH,
+    CLOSE_LOWER_GLYPH,
+
+    OPEN_UPPER_GLYPH,
+    CLOSE_UPPER_GLYPH
+}
